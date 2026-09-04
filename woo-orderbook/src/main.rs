e7dec019be055110df3b947ee99715181052b05c
Dragonado <29285950+Dragonado@users.noreply.github.com>
2026-09-04 02:38:21 -0400

    some stuff

--- woo-orderbook/src/main.rs
+++ woo-orderbook/src/main.rs
@@ -1,28 +1,23 @@
-// use http::Uri;
-use tungstenite::{
-    connect,
-    http::Uri,
-    protocol::Message,
-    ClientRequestBuilder,
-};
-use std::time::{SystemTime, UNIX_EPOCH};
 use serde_json::Value;
+use std::time::{SystemTime, UNIX_EPOCH};
+use tungstenite::{ClientRequestBuilder, connect, http::Uri, protocol::Message};
 
-fn main() { 
+fn main() {
     let uri: Uri = "wss://wss.woox.io/v3/public".parse().unwrap();
     let builder = ClientRequestBuilder::new(uri);
     let (mut socket, response) = connect(builder).unwrap();
-    
+
     println!("Websocket creation response = {response:#?}");
 
-    let timestamp = u64::try_from(SystemTime::now()
-    .duration_since(UNIX_EPOCH)
-    .unwrap()
-    .as_millis()).expect("Unix timestamp should fit in u64");
+    let timestamp = u64::try_from(
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis(),
+    )
+    .expect("Unix timestamp should fit in u64");
 
-    let ping_cmd = format!(
-    r#"{{"cmd":"PING","ts":{timestamp}}}"#
-);  
+    let ping_cmd = format!(r#"{{"cmd":"PING","ts":{timestamp}}}"#);
 
     let started = std::time::Instant::now();
     socket.send(Message::text(ping_cmd)).unwrap();
@@ -37,9 +32,9 @@ fn main() {
                 .as_u64()
                 .expect("PONG response should contain an integer time");
 
-                println!("Response = {:#?}", &bytes);
-                println!("Clock diff: {}ms", server_time - timestamp);
-                println!("Measured Latency: {:?}", started.elapsed());
+            println!("Response = {:#?}", &bytes);
+            println!("Clock diff: {}ms", server_time - timestamp);
+            println!("Measured Latency: {:?}", started.elapsed());
         }
         _ => {
             unreachable!();
